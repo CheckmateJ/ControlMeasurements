6764851102174675e95d38bf7ce84fed161b231a
Michał Matej <matej50@gmail.com>
2019-02-06 09:29:26 +0100

    Add-Connect-With-SQL

---
+++ ControlMeasurements/Data/WaterContext.cs
@@ -0,0 +1,17 @@
+﻿using ControlMeasurements.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControlMeasurements.Data
+{
+    public class WaterContext : DbContext
+    {
+        public WaterContext(DbContextOptions<WaterContext> options)
+            : base(options)
+        { }
+        public DbSet<Water> Waters { get; set; }
+    }
+}
---
+++ ControlMeasurements/Migrations/20190206081400_MeasureWater.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ControlMeasurements.Migrations
+{
+    public partial class MeasureWater : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Waters",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Place = table.Column<string>(nullable: true),
+                    Measurement = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Waters", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Waters");
+        }
+    }
+}
