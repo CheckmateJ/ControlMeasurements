291093509749813b6a545d010210d0c6f2e78d20
Michał Matej <matej50@gmail.com>
2019-02-12 12:33:38 +0100

    created linq

--- ControlMeasurements/Controllers/HomeController.cs
+++ ControlMeasurements/Controllers/HomeController.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ControlMeasurements.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ControlMeasurements.Controllers
 {
     public class HomeController : Controller
     {
+        private MeasurementsContext _context;
+        
+
+        public HomeController(MeasurementsContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+
+            PlaceType measure = Convert.ToString(Enum.PlaceType);
+            var querry = _context.Measurements.Select(n => n).ToList();
+            return View(querry);
+            
         }
     }
 }
