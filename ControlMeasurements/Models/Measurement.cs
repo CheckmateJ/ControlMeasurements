61dc55d07c354717c0f4b1429ef8fe56d5bf2acf
Michał Matej <matej50@gmail.com>
2019-02-14 20:02:04 +0100

    DisplayDate

--- ControlMeasurements/Models/Measurement.cs
+++ ControlMeasurements/Models/Measurement.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ControlMeasurements.Models
 {
@@ -8,5 +9,12 @@ namespace ControlMeasurements.Models
         public PlaceType PlaceType { get; set; }
         public MeasurementType MeasurementType { get; set; }
         public int Value { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime MeasurementDate { get; set; }
+
+        public Measurement()
+        {
+            MeasurementDate = DateTime.Now;
+        }
     }
 } 
