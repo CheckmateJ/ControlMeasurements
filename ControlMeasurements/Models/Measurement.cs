f8735089222b26217c1b5b7dc0291055a09a2abb
Michał Matej <matej50@gmail.com>
2019-02-14 20:13:40 +0100

    CleanUpModel

--- ControlMeasurements/Models/Measurement.cs
+++ ControlMeasurements/Models/Measurement.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
 
 namespace ControlMeasurements.Models
 {
@@ -9,12 +8,10 @@ namespace ControlMeasurements.Models
         public PlaceType PlaceType { get; set; }
         public MeasurementType MeasurementType { get; set; }
         public int Value { get; set; }
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime MeasurementDate { get; set; }
-
         public Measurement()
         {
             MeasurementDate = DateTime.Now;
         }
     }
-} 
+}
