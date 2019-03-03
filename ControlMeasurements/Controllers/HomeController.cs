ff47831ae72d81f320b0e7a3d0e4e3dd7b1e31b6
Michał Matej <matej50@gmail.com>
2019-03-03 23:08:51 +0100

    Display and compute card in cards

--- ControlMeasurements/Controllers/HomeController.cs
+++ ControlMeasurements/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 ﻿using ControlMeasurements.Data;
 using ControlMeasurements.ViewModels;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace ControlMeasurements.Controllers
@@ -40,35 +39,16 @@ namespace ControlMeasurements.Controllers
                                 })
                                 .ToList();
 
-
-
-            //var measurementViews = measurements
-            //                        .GroupBy(x => new
-            //                        {
-            //                            x.MeasurementType,
-            //                            x.PlaceType
-            //                        }) // group by measurement type
-            //                        .Select(g => new MeasurementViewModel // create a new MeasurementViewModel from each group
-            //                        {
-            //                            MeasurementType = g.Key.MeasurementType,
-            //                            PlaceType = g.Key.PlaceType,
-            //                            MeasurementViews = g.OrderByDescending(x => x.Date)
-            //                                                .Take(3) // take three most recent measurements
-            //                                                .Select(x => new MeasurementView // create a new MeasurementView from every Measurement (without "Change" field for now)
-            //                                                {
-            //                                                    Measurement = x
-            //                                                })
-            //                                                .ToList(),
-            //                        }).ToList();
-
-            //foreach (var measurementView in measurementViews)
-            //{
-            //    for (int i = 0; i < measurementView.MeasurementViews.Count - 1; i++) // calculate the change for all measurements without the last one
-            //    {
-            //        measurementView.MeasurementViews[i].Change = measurementView.MeasurementViews[i].Measurement.Value - measurementView.MeasurementViews[i + 1].Measurement.Value;
-            //    }
-            //}
-
+            foreach (var measurement in cards)
+            {
+                foreach (var subcard in measurement.Subcards)
+                {
+                    for (int i = 0; i < subcard.MeasurementViews.Count - 1; i++)
+                    {
+                        subcard.MeasurementViews[i].Change = subcard.MeasurementViews[i].Measurement.Value - subcard.MeasurementViews[i + 1].Measurement.Value;
+                    }
+                }
+            }
             return View(cards);
         }
     }
